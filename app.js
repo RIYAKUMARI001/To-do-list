8a3fc0d6035c0475ba94ba34d15968b949bc7345
Riya kumari <riya010206@gmail.com>
2024-11-20 16:10:10 +0530

    add files

--- app.js
+++ app.js
@@ -4,24 +4,21 @@ let inp = document.querySelector("input");
 
 btn.addEventListener("click", function () {
     let item = document.createElement("li");
-    item.innerText = inp.value;
-
-
+    item.textContent = inp.value;
     let delBtn = document.createElement("button");
-    delBtn.innerText = "Delete";
+    delBtn.textContent = "delete";
     delBtn.classList.add("delete");
     item.appendChild(delBtn);
     ul.appendChild(item);
-    inp.value ="";
+    inp.value = "";
 });
 
-let delBtn = document.querySelectorAll(".delete");
-for (delBtn of delBtns) {
-    delBtn.addEventListener("click", function() {
-       let par = this.parentElement;
-       console.log(par);
+ul.addEventListener("click", function (event) {
+   if (event.target.nodeName == "BUTTON") {
+     let listItem = event.target.parentElement;
+     listItem.remove();
+    
+    //  console.log("deleted");
+   }
+    
 });
-}
-
-
-
