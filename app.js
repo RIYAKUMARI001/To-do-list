558c611a17fd105a2c6aaaae0d09563e860705f5
Riya kumari <riya010206@gmail.com>
2024-11-27 13:56:26 +0530

    first

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ let ul = document.querySelector("ul");
 let inp = document.querySelector("input");
 
 btn.addEventListener("click", function () {
+  if (inp.value.trim() === "") {
+    alert("Please enter a task.");
+    return;
+}
     let item = document.createElement("li");
-    item.textContent = inp.value;
+     item.textContent.add = ("task-item");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    item.appendChild(checkbox);
+    item.appendChild(document.createTextNode(inp.value));
     let delBtn = document.createElement("button");
     delBtn.textContent = "delete";
     delBtn.classList.add("delete");
